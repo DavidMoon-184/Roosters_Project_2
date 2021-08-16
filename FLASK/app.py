095f16ce95362ce733870438c379f420c8679985
DavidMoon-184 <53849210+DavidMoon-184@users.noreply.github.com>
2021-08-16 00:50:17 -0700

    Add files via upload

--- FLASK/app.py
+++ FLASK/app.py
@@ -73,4 +73,4 @@ def get_primary_completion():
         
 
 if __name__ == "__main__":
-    app.run(host='10.5.0.2', port=8080, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
